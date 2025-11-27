61553fbcaff8df0ab8c4df635e4ecd4e31c86515
nponomarenko04 <ponomrenko_nikita_04@mail.ru>
2025-11-27 12:37:29 +0500

    Практикуемся в реализации PageObject  Практикуемся в написании автотестов с использованием POM

--- conftest.py
+++ conftest.py
@@ -1,3 +1,68 @@
-pytest_plugins = (
-    "fixtures.browsers"
-)
+import pytest
+from playwright.sync_api import Playwright, Page, expect
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(
+        storage_state="browser-state.json",
+        viewport={'width': 1920, 'height': 1080}  # Добавьте эту строку
+    )
+    page = context.new_page()
+
+
+    button_registration = page.get_by_test_id("registration-page-registration-button")
+    password_input = page.get_by_test_id("registration-form-password-input").locator("input")
+    username_input = page.get_by_test_id("registration-form-username-input").locator("input")
+    email_input = page.get_by_test_id('registration-form-email-input').locator("input")
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input.fill('user.name@gmail.com')
+    username_input.fill('username')
+    password_input.fill('password')
+    button_registration.click()
+
+    expect(page.locator('[data-testid="navigation-navbar-app-title-text"]')).to_be_visible()
+
+    context.storage_state(path='browser-state.json')
+    browser.close()
+
+
+@pytest.fixture(scope="function")
+def auth_page(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(
+        storage_state="browser-state.json",
+        viewport={'width': 1920, 'height': 1080}  # Добавьте эту строку
+    )
+
+    page = context.new_page()
+
+    yield page
+    browser.close()
+
+
+@pytest.fixture(scope="function")
+def registration_page(page):
+    return RegistrationPage(page)
+
+
+@pytest.fixture(scope="function")
+def dashboard_page(auth_page):
+    return DashboardPage(auth_page)
+
+
+@pytest.fixture(scope="function")
+def courses_list_page(auth_page):
+    return CoursesListPage(auth_page)
+
+
+@pytest.fixture(scope="function")
+def create_course_page(auth_page):
+    return CreateCoursePage(auth_page)
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,17 +1,47 @@
-from playwright.sync_api import sync_playwright, expect,Page
 import pytest
+from playwright.sync_api import Page
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+import time
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state: Page):
-    page = chromium_page_with_state
-    empty_courses_icon = page.get_by_test_id('courses-list-empty-view-icon')
-    empty_courses_list = page.get_by_test_id('courses-list-empty-view-title-text')
-    empty_courses_text = page.get_by_test_id('courses-list-empty-view-description-text')
-    courses_logo = page.get_by_test_id('courses-list-toolbar-title-text')
-
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-    expect(courses_logo).to_be_visible()
-    expect(empty_courses_icon).to_be_visible()
-    expect(empty_courses_list).to_have_text("There is no results")
-    expect(empty_courses_text).to_have_text("Results from the load test pipeline will be displayed here")
+def test_create_course(
+        create_course_page: CreateCoursePage,
+        courses_list_page: CoursesListPage,
+        auth_page: Page
+):
+    auth_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_create_course_form(
+        title="",
+        estimated_time="",
+        description="",
+        max_score="0",
+        min_score="0"
+    )
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image("./testdata/files/image.jpg")
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.fill_create_course_form(
+        title="Playwright",
+        estimated_time="2 weeks",
+        description="Playwright",
+        max_score="100",
+        min_score="10"
+    )
+    create_course_page.click_create_course_button()
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+        title="Playwright",
+        index=0,
+        max_score="100",
+        min_score="10",
+        estimated_time="2 weeks"
+    )
